1ef8ea4cd59314abf6d71bcbd88a124d17647d05
Yuuki Takao <takaaaao3@gmail.com>
2022-10-30 18:03:39 +0900

    clear basc_step7

---
+++ string_primer/basic_step7/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define STR_SIZE 100
+#define N_SIZE 100
+
+int	main(void)
+{
+	int		N;
+	char	S[STR_SIZE * N_SIZE];
+
+	scanf("%d", &N);
+	// printf("%d\n", N);
+
+	for (int i = 0; i < N; i++)
+	{
+		char	S_i[STR_SIZE];
+		scanf("%s", S_i);
+		// printf("%s\n", S_i);
+		strcat(S, S_i);
+	}
+	printf("%s", S);
+
+	return (0);
+}
