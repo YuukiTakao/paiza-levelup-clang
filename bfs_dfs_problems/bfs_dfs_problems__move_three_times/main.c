56f85f3cac4b0413db0d7ca9c8f90f1a1df1ce55
Yuuki Takao <takaaaao3@gmail.com>
2023-01-02 00:40:47 +0900

    refactor

--- bfs_dfs_problems/bfs_dfs_problems__move_three_times/main.c
+++ bfs_dfs_problems/bfs_dfs_problems__move_three_times/main.c
@@ -1,18 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int	max_int(const int a, const int b) {
-	if (a <= b)
-		return (b);
-	else
-		return (a);
-}
+typedef struct Pair {
+	int first;
+	int second;
+}	Pair;
 
-int	min_int(const int a, const int b) {
-	if (a <= b)
-		return (a);
-	else
-		return (b);
+typedef Pair QUEUE_TYPE;
+typedef struct Queue {
+	QUEUE_TYPE	*array;
+	int			last_index;
+	int			cap;
+	int			front_idx;
+}	Queue;
+Queue	*new(int cap) {
+	Queue *self = calloc(1, sizeof(Queue));
+	if (!self)
+		printf("failed Queue new...\n");
+	self->cap = cap;
+	self->last_index = 0;
+	self->front_idx = 0; 
+	self->array = calloc(self->cap, sizeof(QUEUE_TYPE));
+	if (self->array == NULL) {
+		printf("failed array memory allocate...\n");
+		free(self);
+		return NULL;
+	}
+	return self;
+}
+Queue	*resize(Queue *self, int new_cap) {
+	int new_size = new_cap * sizeof(QUEUE_TYPE);
+	QUEUE_TYPE *tmp = realloc(self->array, new_size);
+		if (tmp == NULL) {
+		printf("failed array realloc...\n");
+		return NULL;
+	}
+	self->array = tmp;
+	self->cap = new_cap;
+	return self;
+}
+void	pop(Queue *self) {
+	self->front_idx++;
+}
+int	empty(Queue *self) {
+	return 0 >= (self->last_index - self->front_idx);
+}
+int	queue_size(Queue *self) {
+	return self->last_index - self->front_idx;
+}
+QUEUE_TYPE	front(Queue *self) {
+	return self->array[self->front_idx];
+}
+void	push_back(Queue *self, QUEUE_TYPE elem) {
+	if (self->last_index >= self->cap) {
+		if (resize(self, self->cap * 2) == NULL) {
+			printf("failed push_back in realloc...\n");
+		}
+	}
+	// self->array[self->last_index] = calloc(strlen(elem), sizeof(char));
+	self->array[self->last_index] = elem;
+	self->last_index++;
 }
 
 typedef char TDARRAY_TYPE;
@@ -49,39 +96,45 @@ int	main(void) {
 		for (int j = 0; j < W; j++) {
 			visited[i][j] = -1;
 		}
-		
 	}
 	visited[y][x] = 0;
 
+	Queue *que = new(4);
+	Pair start;
+	start.first = y;
+	start.second = x;
+
+	push_back(que, start);
 	
-	for (int k = 0; k < 3; k++) {
-		for (int i = 0; i < H; i++) {
-			for (int j = 0; j < W; j++) {
-				if (k == visited[i][j]) {
-					int ytop = i - 1;
-					if (0 <= ytop) {
-						map[ytop][j] = '*';
-						visited[ytop][j] = visited[i][j] + 1;
-					}
-					int ybot = i + 1;
-					if (ybot <= H - 1) {
-						map[ybot][j] = '*';
-						visited[ybot][j] = visited[i][j] + 1;
-					}
-					int xleft = j - 1;
-					if (0 <= xleft) {
-						map[i][xleft] = '*';
-						visited[i][xleft] = visited[i][j]+ 1;
-					}
-					int xright = j + 1;
-					if (xright <= W - 1) {
-						map[i][xright] = '*';
-						visited[i][xright] = visited[i][j] + 1;
-					}
-				}
+	while (! empty(que)) {
+		Pair now;
+		now = front(que);
+		pop(que);
+
+		int ny = now.first;
+		int nx = now.second;
+
+		if (visited[ny][nx] == 3)
+			continue;
+		
+		for (int i = -1; i <= 1; i += 2) {
+			if (0 <= ny + i && ny + i < H && map[ny + i][nx] == '.') {
+				Pair next;
+				next.first = ny + i;
+				next.second = nx;
+				push_back(que, next);
+				visited[ny + i][nx] = visited[ny][nx] + 1;
+				map[ny + i][nx] = '*';
+			}
+			if (0 <= nx + i && nx + i < W && map[ny][nx + i] == '.') {
+				Pair next;
+				next.first = ny;
+				next.second = nx + i;
+				push_back(que, next);
+				visited[ny][nx + i] = visited[ny][nx] + 1;
+				map[ny][nx + i] = '*';
 			}
 		}
-		
 	}
 
 	for (int i = 0; i < H; i++) {
