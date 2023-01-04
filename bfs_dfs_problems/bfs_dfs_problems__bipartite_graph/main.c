df3ba32ee29562101d01efda18fb062f87d070d8
Yuuki Takao <takaaaao3@gmail.com>
2023-01-04 23:42:23 +0900

    clear bfs_dfs_problems__bipartite_graph

---
+++ bfs_dfs_problems/bfs_dfs_problems__bipartite_graph/main.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+	int	vertex; // 頂点
+	int	*arr;
+	int	len;
+	int	cap;
+}	AdList;
+AdList	*resize_adlist(AdList *self, int cap) {
+	int byte = sizeof(int);
+	int size = cap * byte;
+	int *tmp = realloc(self->arr, size);
+	if (!tmp)
+		return NULL;
+	self->arr = tmp;
+	self->cap = cap;
+	return self;
+}
+void	push_adlist(AdList *self, int elem) {
+	if (self->len >= self->cap)
+	{
+		if (! resize_adlist(self, self->cap * 2))
+			printf("failed resize memory allocate\n");
+	}
+	self->arr[self->len] = elem;
+	self->len++;
+}
+AdList	*new_adlist(int cap) {
+	AdList *self = calloc(1, sizeof(*self));
+	if (!self)
+		return NULL;
+	self->cap = cap;
+	self->arr = calloc(self->cap, sizeof(int));
+	if (!self->arr) {
+		free(self);
+		return NULL;
+	}
+	return self;
+}
+
+
+int	dfs(AdList **G, int *visited, int now, int color) {
+	visited[now] = color;
+	for (int i = 0; i < G[now]->len; i++) {
+		int next = G[now]->arr[i];
+		if (visited[next] != -1) {
+			if (visited[next] == color) {
+				return 0;
+			}
+			continue;
+		}
+		if (dfs(G, visited, next, 1 - color) == 0) {
+			return 0;
+		}
+	}
+	return 1;
+}
+
+int	main(void) {
+	int N, M;
+	scanf("%d %d", &N, &M);
+	// printf("%d %d\n", N, M);
+
+	AdList **adlist = calloc(N+1, sizeof(AdList *));
+	int a, b;
+	for (int i = 1; i <= M; i++) {
+		scanf("%d %d", &a, &b);
+		if(adlist[a] == NULL) {
+			adlist[a] = new_adlist(2);
+		}
+		if(adlist[b] == NULL) {
+			adlist[b] = new_adlist(2);
+		}
+		push_adlist(adlist[a], b);
+		push_adlist(adlist[b], a);
+	}
+
+	int *visited = calloc(N+1 ,sizeof(int));
+	for (int i = 1; i <= N; i++) {
+		visited[i] = -1;
+	}
+
+	int is_bipartite = 1;
+	for (int i = 1; i <= N; i++) {
+		if (visited[i] != -1) {
+			continue;
+		}
+		if (adlist[i] != NULL && dfs(adlist, visited, i, 0) == 0) {
+			is_bipartite = 0;
+		}
+	}
+	
+	if (is_bipartite == 1)
+		printf("Yes\n");
+	else
+		printf("No\n");
+
+	return (0);
+}
---
+++ bfs_dfs_problems/bfs_dfs_problems__graph_dfs/main.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int	cmp_asc(const void *n1, const void *n2) {
+	if (*(int *)n1 > *(int *)n2)
+		return 1;
+	else if (*(int *)n1 < *(int *)n2)
+		return -1;
+	else
+		return 0;
+}
+typedef struct {
+	int	vertex; // 頂点
+	int	*arr;
+	int	len;
+	int	cap;
+}	AdList;
+AdList	*resize_adlist(AdList *self, int cap) {
+	int byte = sizeof(int);
+	int size = cap * byte;
+	int *tmp = realloc(self->arr, size);
+	if (!tmp)
+		return NULL;
+	self->arr = tmp;
+	self->cap = cap;
+	return self;
+}
+void	push_adlist(AdList *self, int elem) {
+	if (self->len >= self->cap)
+	{
+		if (! resize_adlist(self, self->cap * 2))
+			printf("failed resize memory allocate\n");
+	}
+	self->arr[self->len] = elem;
+	self->len++;
+}
+AdList	*new_adlist(int cap) {
+	AdList *self = calloc(1, sizeof(*self));
+	if (!self)
+		return NULL;
+	self->cap = cap;
+	self->arr = calloc(self->cap, sizeof(int));
+	if (!self->arr) {
+		free(self);
+		return NULL;
+	}
+	return self;
+}
+
+void dfs(AdList **G, int *visited, int position) {
+	visited[position] = 1;
+	printf("%d\n", position);
+	for (int i = 0; i < G[position]->len; i++) {
+		qsort(G[position]->arr, G[position]->len, sizeof(int), cmp_asc);
+		int next = G[position]->arr[i];
+		if (visited[next] == 0) {
+			dfs(G, visited, next);
+		}
+	}
+	return ;
+}
+
+int	main(void) {
+	int N, M, X;
+	scanf("%d %d %d", &N, &M, &X);
+	// printf("%d %d %d\n", N, M, X);
+
+	AdList **adlist = calloc(N+1, sizeof(AdList *));
+	int a, b;
+	for (int i = 1; i <= M; i++) {
+		scanf("%d %d", &a, &b);
+		// printf("%d %d\n", a, b);
+		if (adlist[a] == NULL) {
+			adlist[a] = new_adlist(2);
+		}
+		if (adlist[b] == NULL) {
+			adlist[b] = new_adlist(2);
+		}
+		push_adlist(adlist[a], b);
+		push_adlist(adlist[b], a);
+	}
+
+	if (adlist[X] == NULL) {
+		printf("%d\n", X);
+		return 0;
+	}
+
+	qsort(adlist[X]->arr, adlist[X]->len, sizeof(int), cmp_asc);
+
+	int *visited = calloc(N+1, sizeof(int));
+	
+	dfs(adlist, visited, X);
+
+	return (0);
+}
